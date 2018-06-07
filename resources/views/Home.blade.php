0d38f858cb0173e17b3393a9aa589a58f1472625
lucas41 <delfini.lucas@hotmail.com>
2018-06-07 09:07:11 -0300

    primeiro commit

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,53 @@
+
+<!DOCTYPE html> 
+<html lang="en"> 
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>Monitec</title> 
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <script src="{{ asset('js/app.js') }}" defer></script>
+        <center><img src="/media/logo.jpeg" alt="Logo" style="width:410px;"></center>
+</head>
+<body> 
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark" >
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Site</a>
+         <div class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </div>
+            <div class="collapse navbar-collapse" id="navbar">
+                 <ul class="navbar-nav mr-auto">
+
+            <li class="nav-item">
+                <a class="nav-link" href"#"> Inicio </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href"#"> Seriviço </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href"#"> Perfil </a>
+            </li>
+                </ul>
+
+                       <ul class="navbar-nav ml-auto">
+
+            <li class="nav-item">
+                <a class="nav-link" href"#"> Login </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href"#"> Cadastro </a>
+            </li>
+        </ul>
+            
+</div>
+
+</div>
+    </nav>
+
+</body >
+
+</hmtl>
+
+
+
