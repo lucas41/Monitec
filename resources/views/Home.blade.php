8f0f808cc2143dbbac2e3b3e56feee9ceb6685d6
lucas41 <delfini.lucas@hotmail.com>
2018-06-11 11:45:37 -0300

    commitando dnv

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -8,10 +8,14 @@
         <title>Monitec</title> 
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <script src="{{ asset('js/app.js') }}" defer></script>
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
         <center><img src="/media/logo.jpeg" alt="Logo" style="width:410px;"></center>
 </head>
 
-<body> 
+<body>
+
+
 <nav class="navbar navbar-expand-sm navbar-dark bg-dark" >
 
     <div class="container-fluid">
@@ -48,29 +52,43 @@
 </div>
     </nav>
 <br>
- 
-<div id="carosel" class="carousel slide" data-ride="carosel">
-    <div class="container">
-     
-	<div class="carousel-inner">
-            
-	<div class="carousel-item active">
-			<img src="/media/01.jpg" class="img-fluid d-block">
-	</div>
-	
-	<div class="carousel-item">
-			<img src="/media/00.jpg" class="img-fluid d-block">
-	</div>
-	
-	<div class="carousel-item">
-			<img src="/media/01.jpg" class="img-fluid d-block">
-	</div>
-	
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+<div class="container-fluid">
+<!-- Indicators -->
+<ul class="carousel-indicators">
+  <li data-target="#demo" data-slide-to="0" class="active"></li>
+  <li data-target="#demo" data-slide-to="1"></li>
+  <li data-target="#demo" data-slide-to="2"></li>
+</ul>
+
+<!-- The slideshow -->
+<div class="carousel-inner">
+<div class="container">
+  <div class="carousel-item active">
+	<img src="/media/01.jpg" >
+  </div>
+  <div class="carousel-item">
+	<img src="/media/01.jpg" >
+  </div>
+  <div class="carousel-item">
+	<img src="/media/01.jpg" >
+
+  </div>
+</div>
+
+<!-- Left and right controls -->
+<a class="carousel-control-prev" href="#demo" data-slide="prev">
+  <span class="carousel-control-prev-icon"></span>
+</a>
+<a class="carousel-control-next" href="#demo" data-slide="next">
+  <span class="carousel-control-next-icon"></span>
+</a>
+
+</div> 
 </div>
 </div>
- <br>
- 
- </div>
+<br>
  <div class="container">
 <div class="row mb-5">
 
