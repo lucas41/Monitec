ac45182e648f4aedf185ae5bd7f287765636cd6e
lucas41 <delfini.lucas@hotmail.com>
2018-07-25 13:27:21 -0300

    login pronto

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,5 @@
 Route::get('/', 'MonitecController@index');
 Route::get('/login', 'login@index');
 Auth::routes();
-
-
-
-
-
-
-Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
+
